3a19d3618f3280fb03eddd651ad8005dd0029594
Mate-Ber <matemateberishvili@gmail.com>
2026-03-03 14:50:25 +0700

    add donation table and model

---
+++ database/migrations/2026_03_03_074915_create_donations_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->string('campaign_id')->nullable();   // can be simple text/id for now
+            $table->unsignedInteger('amount');          // in cents
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donations');
+    }
+};
