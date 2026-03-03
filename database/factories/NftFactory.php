222c5be8383c8dcba44e5f99871595c23eb67873
Mate-Ber <matemateberishvili@gmail.com>
2026-03-03 14:56:05 +0700

    add Nft factory

---
+++ database/factories/NftFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Nft>
+ */
+class NftFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(2),
+            'blockchain_id' => $this->faker->uuid(),
+            'price' => $this->faker->numberBetween(100, 20_000),
+        ];
+    }
+}
