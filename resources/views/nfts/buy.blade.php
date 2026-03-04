3fb4fd148b4dbeaccb6af71fa0c912e375b9e9d3
Mate-Ber <matemateberishvili@gmail.com>
2026-03-04 14:22:45 +0700

    use layout in nft page

--- resources/views/nfts/buy.blade.php
+++ resources/views/nfts/buy.blade.php
@@ -1,21 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Buy NFT</title>
-</head>
-<body>
-<h1>Buy "{{ $nft->name }}"</h1>
+@extends('layouts.app', ['title' => 'Buy NFT'])
 
-<p>
-    Blockchain ID: {{ $nft->blockchain_id ?? 'none' }}<br>
-    Price: {{ number_format($nft->price / 100, 2) }} $
-</p>
+@section('content')
+    <h1>Buy "{{ $nft->name }}"</h1>
 
-<form method="POST" action="{{ route('nfts.purchase', $nft) }}">
-    @csrf
-    <p>This is a test payment. No real money is charged.</p>
-    <button type="submit">Confirm test purchase</button>
-</form>
-</body>
-</html>
+    <p>
+        Blockchain ID: {{ $nft->blockchain_id ?? 'none' }}<br>
+        Price: {{ number_format($nft->price / 100, 2) }} $
+    </p>
+
+    <form method="POST" action="{{ route('nfts.purchase', $nft) }}">
+        @csrf
+
+        <p>This is a test payment. No real money is charged.</p>
+
+        <button type="submit">Confirm test purchase</button>
+    </form>
+@endsection
--- resources/views/nfts/index.blade.php
+++ resources/views/nfts/index.blade.php
@@ -1,29 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Nothing NFTs</title>
-</head>
-<body>
-<h1>Nothing NFTs</h1>
+@extends('layouts.app', ['title' => 'Nothing NFTs'])
 
-@if (session('status'))
-    <p style="color: green;">{{ session('status') }}</p>
-@endif
+@section('content')
+    <h1>Nothing NFTs</h1>
 
-@if ($nfts->isEmpty())
-    <p>No NFTs yet.</p>
-@else
-    <ul>
-        @foreach ($nfts as $nft)
-            <a href="{{ route('nfts.buy', $nft) }}">Buy this NFT</a>
-            <li>
-                <strong>{{ $nft->name }}</strong><br>
-                Blockchain ID: {{ $nft->blockchain_id ?? 'none' }}<br>
-                Price: {{ number_format($nft->price / 100, 2) }} $
-            </li>
-        @endforeach
-    </ul>
-@endif
-</body>
-</html>
+    @if ($nfts->isEmpty())
+        <p>No NFTs yet.</p>
+    @else
+        <ul>
+            @foreach ($nfts as $nft)
+                <li>
+                    <strong>{{ $nft->name }}</strong><br>
+                    Blockchain ID: {{ $nft->blockchain_id ?? 'none' }}<br>
+                    Price: {{ number_format($nft->price / 100, 2) }} $<br>
+                    <a href="{{ route('nfts.buy', $nft) }}">Buy this NFT</a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+@endsection
