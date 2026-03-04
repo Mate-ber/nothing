97dc34d51d0d1fc80dc339d984de087bf152fdd2
Mate-Ber <matemateberishvili@gmail.com>
2026-03-04 14:18:54 +0700

    add app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,24 @@
-<x-layouts::app.sidebar :title="$title ?? null">
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
-</x-layouts::app.sidebar>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{{ $title ?? 'Nothing' }}</title>
+</head>
+<body>
+    <nav>
+        <a href="{{ route('home') }}">Home</a> |
+        <a href="{{ route('certificates.index') }}">Certificates</a> |
+        <a href="{{ route('nfts.index') }}">NFTs</a> |
+        <a href="{{ route('donations.create') }}">Donate</a> |
+        <a href="{{ route('payments.index') }}">Payments</a>
+    </nav>
+
+    <hr>
+
+    @if (session('status'))
+        <p style="color: green;">{{ session('status') }}</p>
+    @endif
+
+    @yield('content')
+</body>
+</html>
